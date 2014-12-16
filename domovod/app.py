b5ed2bbe9fbb4d8b4fc041389c6cde8171b12f26
Mystic-Mirage <mm@m10e.net>
2014-12-16 23:45:01 +0200

    Add padding and spacing

--- domovod/app.py
+++ domovod/app.py
@@ -8,7 +8,7 @@ from domovod.widgets.relay import Relay
 class DomoVodApp(App):
 
     def build(self):
-        layout = GridLayout(cols=2)
+        layout = GridLayout(cols=2, padding=10, spacing=10)
         dimmer1 = Dimmer('Dimmer 1')
         dimmer2 = Dimmer('Dimmer 2')
         dimmer3 = Dimmer('Dimmer 3')
